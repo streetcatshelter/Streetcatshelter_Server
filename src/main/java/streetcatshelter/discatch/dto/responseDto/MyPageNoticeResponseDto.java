5671cbac199e6d4c385058eee6f38adb7410cfd2
LIZ\kis30 <kis3053@gmail.com>
2021-10-07 19:30:55 +0900

    cat 동네별 조회 responseDto로 변경
    stackoverflow 에러 안떠요~

--- src/main/java/streetcatshelter/discatch/dto/responseDto/MyPageNoticeResponseDto.java
+++ src/main/java/streetcatshelter/discatch/dto/responseDto/MyPageNoticeResponseDto.java
@@ -1,4 +1,13 @@
 package streetcatshelter.discatch.dto.responseDto;
 
+import lombok.Getter;
+import streetcatshelter.discatch.domain.Notice;
+
+@Getter
 public class MyPageNoticeResponseDto {
+    private Notice notice;
+
+    public MyPageNoticeResponseDto(Notice notice) {
+        this.notice = notice;
+    }
 }
--- src/main/java/streetcatshelter/discatch/service/MyPageService.java
+++ src/main/java/streetcatshelter/discatch/service/MyPageService.java
@@ -5,12 +5,15 @@ import org.springframework.stereotype.Service;
 import streetcatshelter.discatch.domain.*;
 import streetcatshelter.discatch.dto.requestDto.UserInformationRequestDto;
 import streetcatshelter.discatch.dto.responseDto.MyPageCatsResponseDto;
+import streetcatshelter.discatch.dto.responseDto.MyPageNoticeResponseDto;
+import streetcatshelter.discatch.dto.responseDto.MyPageUserInformationResponseDto;
 import streetcatshelter.discatch.oauth.entity.UserPrincipal;
 import streetcatshelter.discatch.repository.CatDetailRepository;
-import streetcatshelter.discatch.repository.CatRepository;
 import streetcatshelter.discatch.repository.LikedRepository;
 import streetcatshelter.discatch.repository.NoticeRepository;
+import streetcatshelter.discatch.repository.UserRepository;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,15 +22,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MyPageService {
 
-    private final CatRepository catRepository;
     private final CatDetailRepository catDetailRepository;
     private final LikedRepository likedRepository;
     private final NoticeRepository noticeRepository;
+    private final UserRepository userRepository;
 
-    public ArrayList<MyPageCatsResponseDto> findAllCats(UserPrincipal userPrincipal) {
+    public List<MyPageCatsResponseDto> findAllCats(UserPrincipal userPrincipal) {
         Long userSeq = userPrincipal.getUser().getUserSeq();
         List<Liked> LikedList = likedRepository.findAllByUser_UserSeq(userSeq);
-        ArrayList<MyPageCatsResponseDto> responseDtoList = new ArrayList<>();
+        List<MyPageCatsResponseDto> responseDtoList = new ArrayList<>();
 
         for(Liked liked: LikedList) {
             Cat cat = liked.getCat();
@@ -49,20 +52,35 @@ public class MyPageService {
     }
 
 
-    public List<Notice> getAllNotices() {
+    public List<MyPageNoticeResponseDto> getAllNotices() {
         List<Notice> noticeList = noticeRepository.findAllByOrderByIdDesc();
-        return noticeList;
+        List<MyPageNoticeResponseDto> responseDtoList = new ArrayList<>();
+        for(Notice notice : noticeList) {
+            MyPageNoticeResponseDto myPageNoticeResponseDto = new MyPageNoticeResponseDto(notice);
+            responseDtoList.add(myPageNoticeResponseDto);
+        }
+        return responseDtoList;
     }
 
-    public String putUserInformation(UserPrincipal userPrincipal, UserInformationRequestDto requestDto) {
+    public MyPageNoticeResponseDto getNotice(Long noticeId) {
+        Notice notice = noticeRepository.findById(noticeId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 notice는 존재하지 않습니다"));
+        return new MyPageNoticeResponseDto(notice);
+    }
+
+    @Transactional
+    public MyPageUserInformationResponseDto putUserInformation(UserPrincipal userPrincipal, UserInformationRequestDto requestDto) {
         User user = userPrincipal.getUser();
         if(requestDto.getNickname() == null) {
-            return "닉네임을 입력해 주세요";
-        } else if (requestDto.getLocation() ==null) {
-            return "최소 동네 1곳은 저장해야 합니다";
+            return new MyPageUserInformationResponseDto("닉네임을 입력해주세요");
+        } else if (requestDto.getLocation() == null && requestDto.getLocation2() == null && requestDto.getLocation3()==null) {
+            return new MyPageUserInformationResponseDto("최소 1곳 이상의 동네를 입력해주세요");
         } else {
             user.update(requestDto);
-            return "회원정보 등록/변경이 완료되었습니다";
+            userRepository.save(user);
+            return new MyPageUserInformationResponseDto("회원 정보 등록/수정이 완료되었습니다.");
         }
     }
+
+
 }
