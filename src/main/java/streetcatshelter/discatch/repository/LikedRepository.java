08b81c5ae907186619b1ca49dc78b3b2aa1379c5
LIZ\kis30 <kis3053@gmail.com>
2021-12-02 22:28:51 +0900

    mypage cat pageable 추가

--- src/main/java/streetcatshelter/discatch/repository/LikedRepository.java
+++ src/main/java/streetcatshelter/discatch/repository/LikedRepository.java
@@ -1,6 +1,8 @@
 package streetcatshelter.discatch.repository;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import streetcatshelter.discatch.domain.Liked;
 
@@ -12,6 +14,7 @@ public interface LikedRepository extends JpaRepository<Liked,Long> {
     Liked findByCatDetailIdAndUser_UserSeq(Long catDetailId, Long userSeq);
     Liked findByCatIdAndUser_UserSeq(Long catId, Long userSeq);
     List<Liked> findAllByUser_UserSeq(Long userSeq);
+    Page<Liked> findAllByUser_UserSeq(Long userSeq, Pageable pageable);
     boolean existsByUser_UserSeqAndCatDetailId(Long userSeq,Long catDetailId);
     int countAllByUser_UserSeq(Long userSeq);
     boolean existsByCatIdAndUser_UserSeq(Long catId, Long userSeq);
